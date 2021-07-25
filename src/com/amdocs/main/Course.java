eb2a97b528a2ae229e2105184afc36f8b3733469
Abhishek Singh <abhisheksingh.nsut@gmail.com>
2021-07-25 18:22:19 +0530

    Course Functions Updated

--- src/com/amdocs/main/Course.java
+++ src/com/amdocs/main/Course.java
@@ -1,5 +1,10 @@
 package com.amdocs.main;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
 public class Course {
 	int Course_id;
 	String Course_name;
@@ -9,6 +14,42 @@ public class Course {
 	
 	public int getCourse_id() {
 		return Course_id;
+	}
+
+	protected String getCourse_name() {
+		return Course_name;
+	}
+
+	protected void setCourse_name(String course_name) {
+		Course_name = course_name;
+	}
+
+	protected String getCourser_esource() {
+		return Courser_esource;
+	}
+
+	protected void setCourser_esource(String courser_esource) {
+		Courser_esource = courser_esource;
+	}
+
+	protected String getCourse_desc() {
+		return Course_desc;
+	}
+
+	protected void setCourse_desc(String course_desc) {
+		Course_desc = course_desc;
+	}
+
+	protected String getCourse_fee() {
+		return Course_fee;
+	}
+
+	protected void setCourse_fee(String course_fee) {
+		Course_fee = course_fee;
+	}
+
+	protected void setCourse_id(int course_id) {
+		Course_id = course_id;
 	}
 
 	public Course(int course_id) {
@@ -24,19 +65,39 @@ public class Course {
 		Course_fee = course_fee;
 	}
 	
-	public int addCourse() {
+	public int addCourse() throws SQLException {
 		
-		//Add course to PL/SQL database
-		
-		//Set Course id Returned From PL/SQL Database
+		Connection con = DBconnect.dbconn();
+		CallableStatement preparedStatement;
+
+		preparedStatement = con.prepareCall("{?= call addCOURSE(?,?,?,?)}");
+		preparedStatement.setString(2, getCourse_name());
+		preparedStatement.setString(3, getCourse_desc());
+		preparedStatement.setString(4, getCourse_fee());
+		preparedStatement.setString(5, getCourser_esource());
+		preparedStatement.registerOutParameter(1, Types.INTEGER);
+		preparedStatement.execute();
+
+		con.close();
+		setCourse_id(preparedStatement.getInt(1));
 		
 		return Course_id;
 	}
 	
-	public void enrollCourse(int user_id,int Course_id) {
+	public int enrollCourse(int user_id,int Course_id) throws SQLException {
 		
-		// Enroll Course based on user_id and cours_id in Database
+		Connection con = DBconnect.dbconn();
+		CallableStatement preparedStatement;
+
+		preparedStatement = con.prepareCall("{?= enrollCOURSE(?,?)}");
+		preparedStatement.setInt(2, user_id);
+		preparedStatement.setInt(3, Course_id);
+		preparedStatement.registerOutParameter(1, Types.INTEGER);
+		preparedStatement.execute();
+
+		con.close();
 		
+		return preparedStatement.getInt(1);
 	}
 	
 }
